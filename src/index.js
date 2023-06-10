219aeba0c0f01281e6737f3626babfe78809b414
constoner <constantin.kalinin@gmail.com>
2023-06-10 22:07:04 +0300

    repair merging conflict

--- src/index.js
+++ src/index.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const welcome = (
-    <h1 className='title title--main'>Welcome to Sedona!</h1>
-);
-
-const header = (
-    <header className="page-header">
-    <nav className="navigation">
-        <a className="navigation-logo" href="./index.html">
-          visit.SEDONA
-        </a>
-        <ul className="navigation-list">
-            <li className="navigation-item">
-                <a className="navigation-link navigation-current" href="./index.html" aria-current="page">Home</a>
-            </li>
-            <li className="navigation-item">
-                <a className="navigation-link" href="./about.html">About Sedona</a>
-            </li>
-            <li className="navigation-item">
-                <a className="navigation-link" href="./catalog.html">Hotels</a>
-            </li>
-        </ul>
-    </nav>
-</header>
-);
-
-const rootElement = document.getElementById('root');
-rootElement.style.fontFamily = 'sans-serif';
-
-const root = createRoot(rootElement);
-
-root.render(
-    <>
-        {header}
-        <main>
-            {welcome}
-        </main>
-    </>
-);
-=======
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Button } from './components/Button/Button';
@@ -118,5 +75,3 @@ const App = () => {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
->>>>>>> second-lesson
